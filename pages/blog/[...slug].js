44774962cdbd024767bd1b977defcae1f062882f
yusufxcingoz <yusufxcingoz@gmail.com>
2021-03-31 12:02:58 +0300

    some changes donen

--- pages/blog/[...slug].js
+++ pages/blog/[...slug].js
@@ -3,7 +3,7 @@ import { getMdxNode, getMdxPaths } from 'next-mdx/server'
 export default function PostPage({ post }) {
   console.log(post)
   return (
-    <div className="site-container ">
+    <div className="max-w-3xl mx-auto px-12  bg-gray-200 py-4 rounded-3xl ">
       <h2 className="my-10 font-bold text-lg ">{post.frontMatter.title}</h2>
       <p>{post.content}</p>
       <p className="my-4 text-gray-500">{post.frontMatter.date}</p>
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -8,7 +8,7 @@ function BlogPage({ posts }) {
         return (
           <Link href={post.url} className="">
             <article key={post.sort}>
-              <div className=" max-w-3xl mx-auto p-4  mb-8 bg-gray-200 hover:bg-gray-300 duration-300 cursor-pointer rounded-lg">
+              <div className=" max-w-3xl mx-auto py-4 px-12  mb-8 bg-gray-200 hover:bg-gray-300 duration-300 cursor-pointer rounded-3xl">
                 <div className="site-container">
                   <h2 className="text-xl font-bold my-2 ">
                     <a className=""> {post.frontMatter.title}</a>
