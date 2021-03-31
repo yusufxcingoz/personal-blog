c54eaa09bb78af4f4deda4cd4a737269a455960a
yusufxcingoz <yusufxcingoz@gmail.com>
2021-03-31 11:54:20 +0300

    blog-things

--- pages/blog/[...slug].js
+++ pages/blog/[...slug].js
@@ -3,7 +3,7 @@ import { getMdxNode, getMdxPaths } from 'next-mdx/server'
 export default function PostPage({ post }) {
   console.log(post)
   return (
-    <div className="site-container">
+    <div className="site-container ">
       <h2 className="my-10 font-bold text-lg ">{post.frontMatter.title}</h2>
       <p>{post.content}</p>
       <p className="my-4 text-gray-500">{post.frontMatter.date}</p>
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -2,23 +2,28 @@ import { getAllNodes } from 'next-mdx/server'
 import Link from 'next/link'
 
 function BlogPage({ posts }) {
-  console.log(posts)
   return (
-    <div className="site-container">
+    <div className="px-6">
       {posts.map((post) => {
         return (
-          <article key={post.url}>
-            <h2 className="text-xl font-bold my-2 ">
-              <Link href={post.url} className="">
-                <a className=""> {post.frontMatter.title}</a>
-              </Link>
-            </h2>
+          <Link href={post.url} className="">
+            <article key={post.sort}>
+              <div className=" max-w-3xl mx-auto p-4  mb-8 bg-gray-200 hover:bg-gray-300 duration-300 cursor-pointer rounded-lg">
+                <div className="site-container">
+                  <h2 className="text-xl font-bold my-2 ">
+                    <a className=""> {post.frontMatter.title}</a>
+                  </h2>
 
-            <p>{post.frontMatter.excerpt}</p>
+                  <p>{post.frontMatter.excerpt}</p>
 
-            <p className="my-4 text-gray-500">{post.frontMatter.date}</p>
-            <p className=" text-gray-700">{post.frontMatter.author} yazdı.</p>
-          </article>
+                  <p className="my-4 text-gray-500">{post.frontMatter.date}</p>
+                  <p className=" text-gray-700">
+                    {post.frontMatter.author} yazdı.
+                  </p>
+                </div>
+              </div>
+            </article>
+          </Link>
         )
       })}
     </div>
