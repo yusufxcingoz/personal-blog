0630c48d8aa9ef035c39c237de402601e8a3f454
yusufxcingoz <yusufxcingoz@gmail.com>
2021-04-02 19:17:22 +0300

    photo overlays done

--- components/photos.js
+++ components/photos.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import Colcade from 'colcade'
 import A from './a'
 import PageTransition from './page-transition'
+import { AiFillHeart } from 'react-icons/ai'
 
 function Photos({ data }) {
   useLayoutEffect(() => {
@@ -25,10 +26,26 @@ function Photos({ data }) {
       {data.map((item) => {
         return (
           
-          <div key={item.id} className="photos-item mb-8   ">
+          <div key={item.id} className="photos-item mb-8 relative  ">
             <PageTransition>
             <A href={item.links.html} blank>
               <img src={item.urls.regular}/>
+              <div className=" overlay absolute top-0 left-0 w-full h-full flex flex-col justify-center items-center text-white bg-black bg-opacity-25  opacity-0 hover:opacity-100 duration-300 ">
+
+                  <div className="text-center flex  space-x-4 flex-row items-center ">
+                    <AiFillHeart className="mr-1"/>
+                    <div className="ml-1">
+                    {item.likes}
+
+                    </div>
+                  </div>
+                <div className="mt-10 font-medium">
+                  {item.description}
+                  
+                  </div>
+                  
+                  
+              </div>
             </A>
           </PageTransition>
           </div>
